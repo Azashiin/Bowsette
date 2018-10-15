9f2c02f37234898540a3a41e58beb07a2a10d315
Azashiin <34695812+Azashiin@users.noreply.github.com>
2018-10-15 21:37:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client ();
+let xp = require("./xp.json");
 
 const prefix = '<';
 const ownerID = '197821972703477781';
@@ -32,6 +33,35 @@ client.on('message', message => {
         ownerID: ownerID, 
         
     } 
+    
+     let xpAdd = Math.floor(Math.random() * 7) + 8;
+  console.log(xpAdd);
+
+  if(!xp[message.author.id]){
+    xp[message.author.id] = {
+      xp: 0,
+      level: 1
+    };
+  }
+
+
+  let curxp = xp[message.author.id].xp;
+  let curlvl = xp[message.author.id].level;
+  let nxtLvl = xp[message.author.id].level * 300;
+  xp[message.author.id].xp =  curxp + xpAdd;
+  if(nxtLvl <= xp[message.author.id].xp){
+    xp[message.author.id].level = curlvl + 1;
+    let lvlup = new Discord.RichEmbed()
+    .setTitle("Level Up!")
+    .setColor(purple)
+    .addField("New Level", curlvl + 1);
+
+    message.channel.send(lvlup).then(msg => {msg.delete(5000)});
+  }
+  fs.writeFile("./xp.json", JSON.stringify(xp), (err) => {
+    if(err) console.log(err)
+});
+         
     
     let commandFile = require(`./commands/${cmd}.js`); 
     
